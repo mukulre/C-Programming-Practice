54354ada1fe08fa95a4f69fd86c53ef27c502c7b
Mukul Raj P <rajmukul242@gmail.com>
2026-09-01 17:47:15 +0530

    Done variable change

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "Mukul";
+    int characterAge = 30;
+    printf("There once was a man named %s\n", characterName);
+    printf("He is %d years old\n",characterAge);
+    return 0;
+}
